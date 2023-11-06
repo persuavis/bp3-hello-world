02439a430ed0960d63f55d29171efeca59f68603
Wim den Braven <wimdenbraven@persuavis.com>
2023-11-06 15:55:35 -0800

    Use relative path for locale files

--- lib/bp3/hello/world.rb
+++ lib/bp3/hello/world.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 require "i18n"
-I18n.load_path += Dir["#{File.expand_path("config/locales")}/*.yml"]
+load_dir = File.expand_path("#{File.dirname(__FILE__)}/../../../config/locales")
+I18n.load_path += Dir.glob("#{load_dir}/*.{rb,yml}")
 
 require_relative "world/context"
 require_relative "world/version"
